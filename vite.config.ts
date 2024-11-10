cf28f380c1941201aa34fc2ceb72fb7efa530a93
Felix Schlegel <felix.schlegel@gmx.de>
2024-11-10 02:24:53 +0100

    ci: added test reporters and coverage analysis

--- vite.config.ts
+++ vite.config.ts
@@ -3,8 +3,15 @@ import { sveltekit } from "@sveltejs/kit/vite";
 
 export default defineConfig({
     plugins: [sveltekit()],
-
+    // Tell Vitest to use the `browser` entry points in `package.json` files, even though it's running in Node
+    resolve: process.env.VITEST ? { conditions: ["browser"] } : undefined,
     test: {
         include: ["test/**/*.{test,spec}.{js,ts}"],
+        reporters: process.env.GITHUB_ACTIONS ? ["default", "github-actions"] : ["default"],
+        coverage: {
+            enabled: true,
+            cleanOnRerun: true,
+            reporter: process.env.GITHUB_ACTIONS ? ["lcov"] : ["html"],
+        },
     },
 });
