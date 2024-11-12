8f1a5714da95a7ca0c4142e55848d38c7f33601c
Felix Schlegel <felix.schlegel@gmx.de>
2024-11-12 23:20:17 +0100

    feat: added project navigation bar component

---
+++ src/lib/components/custom/ProjectNavigationBar.svelte
@@ -0,0 +1,34 @@
+<script lang="ts">
+    import NavigationBar from "$lib/components/custom/NavigationBar.svelte";
+
+    let { user, project, defaultTabValue } = $props<{
+        user: { firstName: string; lastName: string };
+        project: { id: string; name: string };
+        defaultTabValue: "dashboard" | "papers" | "statistics" | "settings";
+    }>();
+
+    let tabs = [
+        {
+            value: "dashboard",
+            label: "Dashboard",
+            href: `/project/${project.id}/dashboard`,
+        },
+        {
+            value: "papers",
+            label: "Papers",
+            href: `/project/${project.id}/papers`,
+        },
+        {
+            value: "statistics",
+            label: "Statistics",
+            href: `/project/${project.id}/statistics`,
+        },
+        {
+            value: "settings",
+            label: "Settings",
+            href: `/project/${project.id}/settings/general`,
+        },
+    ];
+</script>
+
+<NavigationBar {user} backRef="/" pageTitle={project.name} {tabs} {defaultTabValue} />
