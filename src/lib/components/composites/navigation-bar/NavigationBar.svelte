992af3699d0b766f9679b76b79804cb1aeba6d44
Felix Schlegel <felix.schlegel@gmx.de>
2024-11-14 15:42:38 +0100

    feat: wrapped navigation bar in header and back button + tabs in nav element

--- src/lib/components/composites/navigation-bar/NavigationBar.svelte
+++ src/lib/components/composites/navigation-bar/NavigationBar.svelte
@@ -70,57 +70,63 @@
     ];
 </script>
 
-<Card.Root class="shadow-lg w-fit">
-    <Card.Content class="grid grid-flow-col gap-[0.625rem] items-center px-4 py-3">
-        <DropdownMenu.Root>
-            <DropdownMenu.Trigger>
-                <Avatar.Root>
-                    <Avatar.Fallback>{userInitials}</Avatar.Fallback>
-                </Avatar.Root>
-            </DropdownMenu.Trigger>
-            <DropdownMenu.Content class="w-56">
-                <DropdownMenu.Group>
-                    <DropdownMenu.GroupHeading>
-                        {`${user.firstName} ${user.lastName}`}
-                    </DropdownMenu.GroupHeading>
-                    <DropdownMenu.Separator />
-                    <DropdownMenu.Group>
-                        {#each menuItems as item}
-                            <a href={item.href}>
-                                <DropdownMenu.Item>
-                                    <item.icon class="mr-2 size-4" />
-                                    <span>{item.label}</span>
-                                    <DropdownMenu.Shortcut>{item.shortcut}</DropdownMenu.Shortcut>
-                                </DropdownMenu.Item>
-                            </a>
-                        {/each}
-                    </DropdownMenu.Group>
-                    <DropdownMenu.Separator />
-                    <DropdownMenu.Item>
-                        <LogOut class="mr-2 size-4" />
-                        <span>Sign out</span>
-                        <DropdownMenu.Shortcut>⇧⌘Q</DropdownMenu.Shortcut>
-                    </DropdownMenu.Item>
-                </DropdownMenu.Group>
-            </DropdownMenu.Content>
-        </DropdownMenu.Root>
-        {#if backRef !== undefined}
-            <a href={backRef}><ArrowLeft class="w-6 h-6" /></a>
-        {/if}
-        <!-- Children can be e.g. a title element -->
-        {@render children()}
-        {#if tabs.length > 0}
-            <Tabs.Root value={defaultTabValue}>
-                <Tabs.List>
-                    {#each tabs as tab}
-                        <a href={tab.href}>
-                            <Tabs.Trigger value={tab.value}>
-                                <span class="font-semibold">{tab.label}</span>
-                            </Tabs.Trigger>
-                        </a>
-                    {/each}
-                </Tabs.List>
-            </Tabs.Root>
-        {/if}
-    </Card.Content>
-</Card.Root>
+<header>
+    <Card.Root class="shadow-lg w-fit">
+        <nav>
+            <Card.Content class="grid grid-flow-col gap-[0.625rem] items-center px-4 py-3">
+                <DropdownMenu.Root>
+                    <DropdownMenu.Trigger>
+                        <Avatar.Root>
+                            <Avatar.Fallback>{userInitials}</Avatar.Fallback>
+                        </Avatar.Root>
+                    </DropdownMenu.Trigger>
+                    <DropdownMenu.Content class="w-56">
+                        <DropdownMenu.Group>
+                            <DropdownMenu.GroupHeading>
+                                {`${user.firstName} ${user.lastName}`}
+                            </DropdownMenu.GroupHeading>
+                            <DropdownMenu.Separator />
+                            <DropdownMenu.Group>
+                                {#each menuItems as item}
+                                    <a href={item.href}>
+                                        <DropdownMenu.Item>
+                                            <item.icon class="mr-2 size-4" />
+                                            <span>{item.label}</span>
+                                            <DropdownMenu.Shortcut>
+                                                {item.shortcut}
+                                            </DropdownMenu.Shortcut>
+                                        </DropdownMenu.Item>
+                                    </a>
+                                {/each}
+                            </DropdownMenu.Group>
+                            <DropdownMenu.Separator />
+                            <DropdownMenu.Item>
+                                <LogOut class="mr-2 size-4" />
+                                <span>Sign out</span>
+                                <DropdownMenu.Shortcut>⇧⌘Q</DropdownMenu.Shortcut>
+                            </DropdownMenu.Item>
+                        </DropdownMenu.Group>
+                    </DropdownMenu.Content>
+                </DropdownMenu.Root>
+                {#if backRef !== undefined}
+                    <a href={backRef}><ArrowLeft class="w-6 h-6" /></a>
+                {/if}
+                <!-- Children can be e.g. a title element -->
+                {@render children()}
+                {#if tabs.length > 0}
+                    <Tabs.Root value={defaultTabValue}>
+                        <Tabs.List>
+                            {#each tabs as tab}
+                                <a href={tab.href}>
+                                    <Tabs.Trigger value={tab.value}>
+                                        <span>{tab.label}</span>
+                                    </Tabs.Trigger>
+                                </a>
+                            {/each}
+                        </Tabs.List>
+                    </Tabs.Root>
+                {/if}
+            </Card.Content>
+        </nav>
+    </Card.Root>
+</header>
