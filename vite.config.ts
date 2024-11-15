3da9b9d1ce09a3245eff6c9a06062396fdc11118
Felix Schlegel <felix.schlegel@gmx.de>
2024-11-15 23:13:34 +0100

    ci: added testing-library/svelte for better integration tests

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,16 @@
 import { defineConfig } from "vitest/config";
 import { sveltekit } from "@sveltejs/kit/vite";
+import { svelteTesting } from "@testing-library/svelte/vite";
 
 export default defineConfig({
-    plugins: [sveltekit()],
+    plugins: [sveltekit(), svelteTesting()],
     // Tell Vitest to use the `browser` entry points in `package.json` files, even though it's running in Node
     resolve: process.env.VITEST ? { conditions: ["browser"] } : undefined,
     test: {
-        include: ["test/**/*.{test,spec}.{js,ts}"],
+        include: ["test/**/*.{test,spec}.{js,ts}", "integration/**/*.{test,spec}.{js,ts}"],
         reporters: process.env.GITHUB_ACTIONS ? ["default", "github-actions"] : ["default"],
         environment: "jsdom",
+        setupFiles: ["./vitest-setup.ts"],
         sequence: {
             shuffle: true,
         },
