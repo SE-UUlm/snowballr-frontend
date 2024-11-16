1b20130bb6a8eaff6faec8623a71b553a6a6cd9e
Felix Schlegel <felix.schlegel@gmx.de>
2024-11-16 16:39:47 +0100

    ci: create test directories for different types of tests
    Now each type of test can be run separately

--- playwright.config.ts
+++ playwright.config.ts
@@ -5,7 +5,8 @@ export default defineConfig({
         command: "npm run build && npm run preview",
         port: 4173,
     },
-    testDir: "e2e",
+    testDir: "test/e2e",
     // Opt out of parallel tests on CI.
     workers: process.env.CI ? 1 : undefined,
+    outputDir: "test-results",
 });
--- vite.config.ts
+++ vite.config.ts
@@ -7,18 +7,16 @@ export default defineConfig({
     // Tell Vitest to use the `browser` entry points in `package.json` files, even though it's running in Node
     resolve: process.env.VITEST ? { conditions: ["browser"] } : undefined,
     test: {
-        include: ["test/**/*.{test,spec}.{js,ts}", "integration/**/*.{test,spec}.{js,ts}"],
         reporters: process.env.GITHUB_ACTIONS ? ["default", "github-actions"] : ["default"],
-        environment: "jsdom",
-        setupFiles: ["./vitest-setup.ts"],
         sequence: {
             shuffle: true,
         },
         coverage: {
+            include: ["src/**"],
             provider: "v8",
             enabled: true,
             cleanOnRerun: true,
-            reporter: process.env.GITHUB_ACTIONS ? ["lcov"] : ["html"],
+            reporter: process.env.GITHUB_ACTIONS ? ["lcovonly"] : ["html"],
         },
     },
 });
