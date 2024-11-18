803e9f50cc2b57fb28c8a124fd421fa90dffc570
Felix Schlegel <felix.schlegel@gmx.de>
2024-11-18 18:07:46 +0100

    feat: add paper details card and paper research context card

---
+++ src/lib/components/composites/paper-components/cards/PaperResearchContextCard.svelte
@@ -0,0 +1,28 @@
+<script lang="ts">
+    import PaperCard from "./PaperCard.svelte";
+    import PaperCardContent from "./PaperCardContent.svelte";
+
+    const tabs = [
+        { value: "1", label: "References/Citations" },
+        { value: "2", label: "Review Information" },
+    ];
+</script>
+
+<PaperCard {tabs}>
+    <PaperCardContent value="1">
+        <span
+            >Will be implemented in <a
+                class="text-blue-400"
+                href="https://github.com/SE-UUlm/snowballr-frontend/issues/42">#42</a
+            >.</span
+        >
+    </PaperCardContent>
+    <PaperCardContent value="2">
+        <span
+            >Will be implemented in <a
+                class="text-blue-400"
+                href="https://github.com/SE-UUlm/snowballr-frontend/issues/45">#45</a
+            >.</span
+        >
+    </PaperCardContent>
+</PaperCard>
