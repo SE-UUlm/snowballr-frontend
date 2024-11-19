db31db02597fb54ce18d9e9c14d5ca27ff95d3fd
Leonhard Alkewitz <leonhard@alkewitz.net>
2024-11-19 23:38:56 +0100

    feat: add search bar layout (and therefore adapted input layout)

---
+++ src/lib/components/composites/search-bar/SearchBar.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+    import { Input } from "$lib/components/primitives/input/index";
+    import { Search } from "lucide-svelte";
+
+    interface Props {
+        placeholderText?: string;
+    }
+
+    const { placeholderText = "Search" }: Props = $props();
+</script>
+
+<!-- inspired by https://github.com/shadcn-ui/ui/issues/1562 -->
+<div class="relative w-full">
+    <!-- TODO: check, whether to change the placeholder text styling -->
+    <Input type="text" placeholder={placeholderText} class="px-4 py-3 pr-10" />
+    <Search
+        class="absolute right-4 top-1/2 transform -translate-y-1/2 h-4 w-4 text-muted-foreground"
+    />
+</div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,10 +1,15 @@
 <script lang="ts">
     import SimpleNavigationBar from "$lib/components/composites/navigation-bar/SimpleNavigationBar.svelte";
+    import SearchBar from "$lib/components/composites/search-bar/SearchBar.svelte";
 
     const user = {
         firstName: "Max",
         lastName: "Mustermann",
     };
+
+    const searchPlaceholderText: string = "Search paper";
 </script>
 
 <SimpleNavigationBar {user} title="SnowballR" tabs={[]} defaultTabValue="" />
+
+<SearchBar placeholderText={searchPlaceholderText}></SearchBar>
