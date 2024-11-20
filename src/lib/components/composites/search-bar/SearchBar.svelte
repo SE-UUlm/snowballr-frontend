50ba2018165ada7fdaef9655fd1d370591f2de41
Leonhard Alkewitz <leonhard@alkewitz.net>
2024-11-20 15:20:13 +0100

    feat: add search value binding and handler for search input (WIP: only log input to console)

--- src/lib/components/composites/search-bar/SearchBar.svelte
+++ src/lib/components/composites/search-bar/SearchBar.svelte
@@ -7,12 +7,28 @@
     }
 
     const { placeholderText = "Search" }: Props = $props();
+
+    let searchInput: string = $state("");
+
+    const onSearchInputUpdate = (event: KeyboardEvent) => {
+        // Check, whether user pressed the 'Esc' character to clear input
+        if (event.key == "Escape") {
+            searchInput = "";
+        } else {
+            console.log(searchInput);
+        }
+    };
 </script>
 
 <!-- inspired by https://github.com/shadcn-ui/ui/issues/1562 -->
 <div class="relative w-full">
-    <!-- TODO: check, whether to change the placeholder text styling -->
-    <Input type="text" placeholder={placeholderText} class="px-4 py-3 pr-10" />
+    <Input
+        type="text"
+        placeholder={placeholderText}
+        bind:value={searchInput}
+        onkeyup={onSearchInputUpdate}
+        class="px-4 py-3 pr-10"
+    />
     <Search
         class="absolute right-4 top-1/2 transform -translate-y-1/2 h-4 w-4 text-muted-foreground"
     />
