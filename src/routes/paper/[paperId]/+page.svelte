6f80bbed12ce9a5581ef1ee561e4ca2de2a4ced1
Felix Schlegel <felix.schlegel@gmx.de>
2024-11-21 14:15:45 +0100

    feat: make paper view page a component

--- src/routes/paper/[paperId]/+page.svelte
+++ src/routes/paper/[paperId]/+page.svelte
@@ -1,7 +1,5 @@
 <script lang="ts">
-    import PaperNavigationBar from "$lib/components/composites/navigation-bar/PaperNavigationBar.svelte";
-    import PaperDetailsCard from "$lib/components/composites/paper-components/cards/PaperDetailsCard.svelte";
-    import PaperResearchContextCard from "$lib/components/composites/paper-components/cards/PaperResearchContextCard.svelte";
+    import PaperView from "$lib/components/composites/paper-components/PaperView.svelte";
 
     const { data } = $props();
     const { user, paper } = data;
@@ -10,19 +8,4 @@
 <svelte:head>
     <title>{paper.title}</title>
 </svelte:head>
-<div class="flex flex-row justify-between bg-red-200 h-fit w-full gap-4">
-    <PaperNavigationBar {user} backRef="/" paper={data.paper} />
-    <div class="bg-amber-400">
-        <div class="w-8 h-8 bg-slate-400">BB</div>
-    </div>
-</div>
-<main class="flex flex-col h-full w-full px-2 py-4 gap-5">
-    <div class="flex flex-row w-full h-full gap-5">
-        <PaperDetailsCard />
-        <PaperResearchContextCard />
-    </div>
-    <div class="flex flex-row w-full h-fit justify-between">
-        <div>Button to left</div>
-        <div>Button to right</div>
-    </div>
-</main>
+<PaperView {user} {paper} />
