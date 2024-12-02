3bb05190b0d3009b74420f0fb397ff7086fb4875
Leonhard Alkewitz <leonhard@alkewitz.net>
2024-12-02 12:39:36 +0100

    docs: add component comment for paper entry component (+ prepare onClick method of component)

--- src/lib/components/composites/paper-components/PaperEntry.svelte
+++ src/lib/components/composites/paper-components/PaperEntry.svelte
@@ -6,9 +6,11 @@
     interface PaperEntryProps {
         paper: Paper;
         showReviewStatus: boolean;
+        onClick?: () => void;
     }
+    const onClickHandler = () => {};
 
-    const { paper, showReviewStatus = false }: PaperEntryProps = $props();
+    const { paper, showReviewStatus = false, onClick = onClickHandler }: PaperEntryProps = $props();
 
     const reviewDecisionColor: Record<ReviewDecision | "unreviewed", string> = {
         [ReviewDecision.Accepted]: "border-accept-green",
@@ -18,6 +20,24 @@
     };
 </script>
 
+<!--
+@component
+Container displaying important paper information, optionally with review information.
+
+This component shows the
+  - paper id
+  - title
+  - authors (or 'unknown authors' if none are specified)
+as well as, if not in review mode, the review information about this paper.
+
+Furthermore this component is clickable and navigates to the corresponding paper view,
+if the event handler is not overridden.
+
+Usage:
+```svelte
+    <PaperEntry paper={paper} showReviewStatus={true} />
+```
+-->
 <div
     class="flex flex-row items-center justify-end pe-3 gap-3 w-full border {showReviewStatus
         ? 'border-l-0'
