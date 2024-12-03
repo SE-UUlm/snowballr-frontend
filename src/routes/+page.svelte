451edec884306047eaaa747e9a466944fdc94523
Leonhard Alkewitz <leonhard@alkewitz.net>
2024-12-03 20:33:25 +0100

    feat: add basic html structure for homepage (+ create new list wrapper component, used on the homepage)"

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
     import SimpleNavigationBar from "$lib/components/composites/navigation-bar/SimpleNavigationBar.svelte";
+    import NamedListWrapper from "$lib/components/composites/NamedListWrapper.svelte";
 
     const { data } = $props();
     const { user } = data;
@@ -9,3 +10,15 @@
     <title>SnowballR</title>
 </svelte:head>
 <SimpleNavigationBar {user} title="SnowballR" />
+
+<main class="flex flex-row h-full w-full mb-10 gap-x-5">
+    <NamedListWrapper listName={"Open Reviews"}
+        ><span
+            >Will be implemented in <a
+                class="text-blue-400"
+                href="https://github.com/SE-UUlm/snowballr-frontend/issues/121">#121</a
+            >.</span
+        ></NamedListWrapper
+    >
+    <NamedListWrapper listName={"Projects"}>list content</NamedListWrapper>
+</main>
