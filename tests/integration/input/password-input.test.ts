030177892d6548c80e0055d3c783ed67b626b73a
Felix Schlegel <felix.schlegel@gmx.de>
2024-12-08 13:46:59 +0100

    test: add test case for forgot password link

--- tests/integration/input/password-input.test.ts
+++ tests/integration/input/password-input.test.ts
@@ -5,11 +5,12 @@ import { assert, describe, expect, test } from "vitest";
 describe("PasswordInput", () => {
     const validPassword = "z's?c].e2x<@($\"<#;\\A]]3D@F)/^v^!";
 
-    test("When props are provided, then label and input are shown", () => {
+    test("When props are provided, then label, link and input are shown", () => {
         render(PasswordInput, {
             target: document.body,
             props: {
                 value: validPassword,
+                showForgotPasswordLink: true,
             },
         });
 
@@ -17,6 +18,11 @@ describe("PasswordInput", () => {
         const label = document.getElementsByTagName("label")[0];
         expect(label).toBeInTheDocument();
         expect(label).toHaveTextContent("Password");
+
+        // Link exists
+        const link = document.getElementsByTagName("a")[0];
+        expect(link).toBeInTheDocument();
+        expect(link).toHaveTextContent("Forgot your password?");
 
         // Input exists
         const inputElement = document.getElementById("password-input");
@@ -93,5 +99,17 @@ describe("PasswordInput", () => {
         await waitFor(() => {
             expect(input).toHaveAttribute("type", "password");
         });
+    });
+
+    test("When `showForgotPasswordLink` is false, then link is not shown", () => {
+        render(PasswordInput, {
+            target: document.body,
+            props: {
+                showForgotPasswordLink: false,
+            },
+        });
+
+        const link = document.getElementsByTagName("a");
+        expect(link).toHaveLength(0);
     });
 });
