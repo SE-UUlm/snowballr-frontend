444852b4a5c62966c9b86a3bcbe8df14c589a971
Leonhard Alkewitz <leonhard@alkewitz.net>
2024-12-17 00:35:01 +0100

    test: add tests for project list entry and named list (+ extract calculateStageProgress method to unit test it)

---
+++ tests/integration/project-components/project-list-entry.test.ts
@@ -0,0 +1,46 @@
+import { expect, test, describe } from "vitest";
+import ProjectListEntry from "$lib/components/composites/project-components/ProjectListEntry.svelte";
+import { render, screen } from "@testing-library/svelte";
+import { createProject, Users } from "../../model-builder";
+
+describe("ProjectListEntryComponent", () => {
+    test("When all required props are provided, then the project list entry is completely shown.", () => {
+        render(ProjectListEntry, {
+            props: {
+                project: createProject({
+                    name: "Demo Project",
+                }),
+                members: [Users.johnDoe, Users.janeDoe],
+                stage: 1,
+                stageProgress: 60,
+            },
+        });
+
+        // Project information are shown directly
+        expect(screen.getByText("Demo Project")).toBeInTheDocument();
+        expect(screen.getByText("John Doe, Jane Doe")).toBeInTheDocument();
+        expect(screen.getByText("Stage 1")).toBeInTheDocument();
+
+        // Project (stage) progress is visualized using a progress bar
+        expect(screen.queryByText("60")).not.toBeInTheDocument();
+        expect(screen.queryByTestId("stage-progress-bar")).toHaveValue(60);
+    });
+
+    test("When all no members are provided, then the list entry shows a hint.", () => {
+        render(ProjectListEntry, {
+            props: {
+                project: createProject({
+                    name: "Demo Project",
+                }),
+                members: [],
+                stage: 1,
+                stageProgress: 60,
+            },
+        });
+
+        expect(screen.getByText("Demo Project")).toBeInTheDocument();
+        expect(screen.getByText("no members")).toBeInTheDocument();
+        expect(screen.getByText("Stage 1")).toBeInTheDocument();
+        expect(screen.queryByText("60")).not.toBeInTheDocument();
+    });
+});
---
+++ tests/integration/utils/ExampleSnippets.svelte
@@ -0,0 +1,14 @@
+<script module>
+    import ExampleListItem from "./ExampleListItem.svelte";
+    import ExampleListItemSkeleton from "./ExampleListItemSkeleton.svelte";
+
+    export { listItemComponent, listItemSkeleton };
+</script>
+
+{#snippet listItemComponent(componentData)}
+    <ExampleListItem {...componentData} />
+{/snippet}
+
+{#snippet listItemSkeleton()}
+    <ExampleListItemSkeleton />
+{/snippet}
