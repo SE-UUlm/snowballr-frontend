704b37de36737a1eaf2e49bed4d29a705de6add4
Leonhard Alkewitz <leonhard@alkewitz.net>
2024-12-22 11:39:59 +0100

    fix: add aria label for progress bar

--- src/lib/components/composites/project-components/ProjectListEntry.svelte
+++ src/lib/components/composites/project-components/ProjectListEntry.svelte
@@ -52,6 +52,7 @@ Usage:
             class="h-2.5 w-24 sm:w-28 md:w-48 lg:w-28 xl:w-40 2xl:w-52 bg-slate-200 group-hover/project-list-entry:bg-slate-300"
             value={stageProgress}
             data-testid="stage-progress-bar"
+            aria-label="Stage Progress"
         />
     </div>
 </a>
