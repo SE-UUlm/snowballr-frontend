97eb1255cc699f47f33229b4b9b5a51a00ad932e
Felix Schlegel <felix.schlegel@gmx.de>
2025-01-10 18:03:34 +0100

    feat: add select component

---
+++ src/lib/components/primitives/select/select-scroll-up-button.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+    import ChevronUp from "lucide-svelte/icons/chevron-up";
+    import { Select as SelectPrimitive, type WithoutChildrenOrChild } from "bits-ui";
+    import { cn } from "$lib/utils/shadcn-helper.js";
+
+    let {
+        ref = $bindable(null),
+        class: className,
+        ...restProps
+    }: WithoutChildrenOrChild<SelectPrimitive.ScrollUpButtonProps> = $props();
+</script>
+
+<SelectPrimitive.ScrollUpButton
+    class={cn("flex cursor-default items-center justify-center py-1", className)}
+    bind:ref
+    {...restProps}
+>
+    <ChevronUp class="size-4" />
+</SelectPrimitive.ScrollUpButton>
---
+++ src/lib/components/primitives/select/select-separator.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+    import type { Separator as SeparatorPrimitive } from "bits-ui";
+    import { Separator } from "$lib/components/primitives/separator/index.js";
+    import { cn } from "$lib/utils/shadcn-helper.js";
+
+    let {
+        ref = $bindable(null),
+        class: className,
+        ...restProps
+    }: SeparatorPrimitive.RootProps = $props();
+</script>
+
+<Separator class={cn("bg-muted -mx-1 my-1 h-px", className)} bind:ref {...restProps} />
