3bfdf6fb1c252ded124221400e3ae79537ac3ff1
Felix Schlegel <felix.schlegel@gmx.de>
2025-02-19 12:05:43 +0100

    docs: add documentation for resource wrapper

--- src/lib/resource.svelte.ts
+++ src/lib/resource.svelte.ts
@@ -1,3 +1,35 @@
+/**
+ * Wrapper for a resource state that is loading.
+ *
+ * As long as the promise is pending, the state will have the initial value {@link options.initialValue}.
+ * When the promise resolves, the state will be set to the value returned by {@link options.onSuccess}.
+ * If the promise rejects, the state will be set to {@link options.onErrorValue} if it is defined.
+ *
+ * The value of the state can be accessed via the `value` property.
+ *
+ * Usage:
+ * ```ts
+ * const loadingResource = backendService.getSomeData();
+ *
+ * const state = resource(loadingResource, {
+ *    initialValue: [],
+ *    onSuccess: (data) => data,
+ *    onErrorValue: undefined,
+ * });
+ *
+ * ...
+ *
+ * {#each $state.value as item}
+ *    <div>{item}</div>
+ * {/each}
+ * ```
+ *
+ * @param loadingResource - The promise representing the loading resource.
+ * @param options.initialValue - The initial value of the state.
+ * @param options.onSuccess - The function that is called when the promise resolves and sets the value of the state.
+ * @param options.onErrorValue - The value that is set when the promise rejects.
+ * @returns The state that represents the loading resource.
+ */
 export const resource = <TPromise, TValue>(
     loadingResource: Promise<TPromise>,
     options: {
