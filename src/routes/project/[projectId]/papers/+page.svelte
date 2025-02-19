fcb256da8e101c3f4e5d8efba23a9c0c7a9faa48
Felix Schlegel <felix.schlegel@gmx.de>
2025-02-19 10:01:47 +0100

    feat: add project papers page

--- src/routes/project/[projectId]/papers/+page.svelte
+++ src/routes/project/[projectId]/papers/+page.svelte
@@ -1,8 +1,41 @@
 <script lang="ts">
     import ProjectNavigationBar from "$lib/components/composites/navigation-bar/ProjectNavigationBar.svelte";
+    import PaperListEntry from "$lib/components/composites/paper-components/PaperListEntry.svelte";
+    import SearchBar from "$lib/components/composites/search-bar/SearchBar.svelte";
+    import * as Accordion from "$lib/components/primitives/accordion/index.js";
+    import Button from "$lib/components/primitives/button/button.svelte";
+    import ChevronDown from "lucide-svelte/icons/chevron-down";
+    import ChevronUp from "lucide-svelte/icons/chevron-up";
+    import * as Card from "$lib/components/primitives/card/index.js";
+    import StagesSelect from "./StagesSelect.svelte";
+    import ReviewersSelect from "./ReviewersSelect.svelte";
+    import PublishersSelect from "./PublishersSelect.svelte";
+    import YearsSelect from "./YearsSelect.svelte";
+    import DecisionsSelect from "./DecisionsSelect.svelte";
+    import CriteriaSelect from "./CriteriaSelect.svelte";
+    import { resource } from "$lib/resource.svelte";
 
     let { data } = $props();
-    const { user, projectId, loadingProject } = data;
+    const {
+        user,
+        projectId,
+        loadingProject,
+        loadingCriteria,
+        loadingStages,
+        loadingYears,
+        loadingPublishers,
+        loadingReviewers,
+    } = data;
+
+    const stagesResource = resource(loadingStages, {
+        initialValue: [],
+        onSuccess: (stages) => stages,
+        onErrorValue: [],
+    });
+
+    const loadingStageCount = loadingProject.then((project) => project.maxStage);
+
+    let showFilters = $state(true);
 </script>
 
 <svelte:head>
@@ -14,4 +47,66 @@
         <title>Papers</title>
     {/await}
 </svelte:head>
-<ProjectNavigationBar defaultTabValue="papers" {loadingProject} {projectId} {user} />
+<ProjectNavigationBar {user} {projectId} {loadingProject} defaultTabValue="papers" />
+<main class="flex flex-row h-full w-full px-4 py-2 gap-10">
+    <div class="flex flex-col w-full h-full gap-5">
+        <div class="flex flex-col w-full h-fit gap-2.5">
+            <div class="flex flex-row items-center gap-2.5">
+                <Button onclick={() => (showFilters = !showFilters)}>
+                    Filters
+                    {#if showFilters}
+                        <ChevronUp class="size-4" />
+                    {:else}
+                        <ChevronDown class="size-4" />
+                    {/if}
+                </Button>
+                <SearchBar placeholderText="Search paper" onSearch={() => {}} />
+            </div>
+            {#if showFilters}
+                <div class="flex flex-row items-center gap-2.5 flex-wrap">
+                    <StagesSelect {loadingStageCount} />
+                    <ReviewersSelect {loadingReviewers} />
+                    <PublishersSelect {loadingPublishers} />
+                    <YearsSelect {loadingYears} />
+                    <DecisionsSelect />
+                    <CriteriaSelect {loadingCriteria} />
+                </div>
+            {/if}
+        </div>
+        <div class="w-full h-full">
+            <Accordion.Root type="multiple">
+                {#each stagesResource.value as stage (stage.stageIndex)}
+                    <Accordion.Item value={`stage-${stage.stageIndex}`}>
+                        <Accordion.Trigger>Stage {stage.stageIndex}</Accordion.Trigger>
+                        <Accordion.Content>
+                            <div class="flex flex-col pl-5 gap-4">
+                                {#each stage.papers as paper}
+                                    <PaperListEntry {paper} {projectId} showReviewStatus />
+                                {/each}
+                            </div>
+                        </Accordion.Content>
+                    </Accordion.Item>
+                {/each}
+            </Accordion.Root>
+        </div>
+    </div>
+    <Card.Root class="flex flex-col w-[60%] h-full gap-5 shadow-lg hidden">
+        <Card.Content>
+            <h2>General Information</h2>
+            <div class="flex flex-col gap-2.5">
+                <div class="flex flex-row justify-between items-center">
+                    <h3>Number of Papers</h3>
+                    <p>4</p>
+                </div>
+                <div class="flex flex-row justify-between items-center">
+                    <h3>Number of Reviews</h3>
+                    <p>8</p>
+                </div>
+                <div class="flex flex-row justify-between items-center">
+                    <h3>Number of Decisions</h3>
+                    <p>4</p>
+                </div>
+            </div>
+        </Card.Content>
+    </Card.Root>
+</main>
---
+++ src/routes/project/[projectId]/papers/+page.ts
@@ -0,0 +1,88 @@
+import { backendService } from "$lib/grpc-api";
+import { Project_Paper } from "$lib/model/api/project";
+import { asPaper, type Stage } from "$lib/model/general";
+import type { PageLoad } from "./$types";
+
+export const load: PageLoad = ({ params }) => {
+    const projectId = { id: params.projectId };
+
+    const loadingCriteria = backendService
+        .getAllCriteriaForProject(projectId)
+        .response.then(({ criteria }) => criteria);
+    // attach noop-catch to handle promise rejection correctly (see https://svelte.dev/docs/kit/load#Streaming-with-promises)
+    loadingCriteria.catch(() => {});
+
+    const loadingPapers = backendService
+        .getAllProjectPapersForProject(projectId)
+        .response.then(({ projectPapers }) => projectPapers);
+    // attach noop-catch to handle promise rejection correctly (see https://svelte.dev/docs/kit/load#Streaming-with-promises)
+    loadingPapers.catch(() => {});
+
+    const loadingStages = loadingPapers.then(organizePapersByStage);
+    // attach noop-catch to handle promise rejection correctly (see https://svelte.dev/docs/kit/load#Streaming-with-promises)
+    loadingStages.catch(() => {});
+
+    const loadingYears = loadingPapers.then((projectPapers) => {
+        const allYears = projectPapers.map((projectPaper) => asPaper(projectPaper).year);
+        const uniqueYears = new Set(allYears);
+        return Array.from(uniqueYears);
+    });
+    // attach noop-catch to handle promise rejection correctly (see https://svelte.dev/docs/kit/load#Streaming-with-promises)
+    loadingYears.catch(() => {});
+
+    const loadingPublishers = loadingPapers.then((projectPapers) => {
+        const allPublishers = projectPapers.map((projectPaper) => asPaper(projectPaper).publisher);
+        const uniquePublishers = new Set(allPublishers);
+        return Array.from(uniquePublishers);
+    });
+    // attach noop-catch to handle promise rejection correctly (see https://svelte.dev/docs/kit/load#Streaming-with-promises)
+    loadingPublishers.catch(() => {});
+
+    const loadingMembers = backendService
+        .getProjectMembers({ id: params.projectId })
+        .response.then(({ members }) => members);
+    // attach noop-catch to handle promise rejection correctly (see https://svelte.dev/docs/kit/load#Streaming-with-promises)
+    loadingMembers.catch(() => {});
+
+    const loadingReviewers = Promise.all([loadingPapers, loadingMembers]).then(
+        ([projectPapers, members]) => {
+            const allReviewerIds = projectPapers
+                .flatMap((projectPaper) => projectPaper.reviews)
+                .map((review) => review.userId);
+            const uniqueReviewerIds = new Set(allReviewerIds);
+            return members
+                .map((member) => member.user!)
+                .filter((user) => uniqueReviewerIds.has(user.id));
+        },
+    );
+    // attach noop-catch to handle promise rejection correctly (see https://svelte.dev/docs/kit/load#Streaming-with-promises)
+    loadingReviewers.catch(() => {});
+
+    return {
+        loadingCriteria,
+        loadingStages,
+        loadingYears,
+        loadingPublishers,
+        loadingReviewers,
+    };
+};
+
+/**
+ * Organizes the papers by their stage. The stages are determined by the stageIndex of the papers.
+ *
+ * @param papers - The papers of a project from different stages.
+ * @returns A list of stages with the papers of the respective stage.
+ */
+function organizePapersByStage(papers: Project_Paper[]): Stage[] {
+    const stages: Stage[] = [];
+    for (const paper of papers) {
+        let stage = stages.find((stage) => stage.stageIndex === paper.stage);
+        if (!stage) {
+            stage = { stageIndex: paper.stage, papers: [] };
+            stages.push(stage);
+        }
+
+        stage.papers.push(paper);
+    }
+    return stages;
+}
