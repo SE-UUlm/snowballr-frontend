902211991b250c162d286e29170117094b0603cb
Felix Schlegel <felix.schlegel@gmx.de>
2025-02-25 15:33:57 +0100

    feat: remove left padding when select item is disabled

--- src/lib/components/primitives/select/select-item.svelte
+++ src/lib/components/primitives/select/select-item.svelte
@@ -9,25 +9,30 @@
         value,
         label,
         children: childrenProp,
+        disabled = false,
         ...restProps
     }: WithoutChild<SelectPrimitive.ItemProps> = $props();
 </script>
 
 <SelectPrimitive.Item
     class={cn(
-        "data-highlighted:bg-accent data-highlighted:text-accent-foreground relative flex w-full cursor-default select-none items-center rounded-sm py-1.5 pl-8 pr-2 text-sm outline-hidden data-disabled:pointer-events-none data-disabled:opacity-50",
+        "data-highlighted:bg-accent data-highlighted:text-accent-foreground relative flex w-full cursor-default select-none items-center rounded-sm py-1.5 pr-2 text-sm outline-hidden data-disabled:pointer-events-none data-disabled:opacity-50",
+        disabled ? "pl-2" : "pl-8",
         className,
     )}
+    {disabled}
     {value}
     bind:ref
     {...restProps}
 >
     {#snippet children({ selected, highlighted })}
-        <span class="absolute left-2 flex size-3.5 items-center justify-center">
-            {#if selected}
-                <Check class="size-4" />
-            {/if}
-        </span>
+        {#if !disabled}
+            <span class="absolute left-2 flex size-3.5 items-center justify-center">
+                {#if selected}
+                    <Check class="size-4" />
+                {/if}
+            </span>
+        {/if}
         {#if childrenProp}
             {@render childrenProp({ selected, highlighted })}
         {:else}
