43cad14b0320384d5d7bb9d214814af6cb38b6a5
Felix Schlegel <felix.schlegel@gmx.de>
2025-04-03 15:47:27 +0200

    fix: remove test layouts

--- tests/integration/settings/project-settings/TestProjectSettingslayout.svelte
+++
@@ -1,5 +0,0 @@
-<script lang="ts">
-    import ProjectSettingsLayout from "$lib/components/composites/settings/project-settings/ProjectSettingsLayout.svelte";
-</script>
-
-<ProjectSettingsLayout projectId="1" selectedTab="general">This is a test!</ProjectSettingsLayout>
--- tests/integration/settings/user-settings/TestUserSettingslayout.svelte
+++
@@ -1,5 +0,0 @@
-<script lang="ts">
-    import UserSettingsLayout from "$lib/components/composites/settings/user-settings/UserSettingsLayout.svelte";
-</script>
-
-<UserSettingsLayout selectedTab="account">This is a test!</UserSettingsLayout>
