547b1050917e8c9ab28a0a93a926da302b1ffd48
Leonhard Alkewitz <leonhard@alkewitz.net>
2025-04-14 12:47:26 +0200

    chore: move node version and type stripping flag constants into the run-playwright.js script

--- scripts/run-node.js
+++
@@ -1,7 +0,0 @@
-// Since with Node 23 type stripping no longer has to be explicitly activated,
-// but is standard and must be explicitly deactivated,
-// this script checks whether node is running in version 23 and if so,
-// type stripping is deactivated, since the generated GRPC client does not work with type stripping.
-
-export const isNode23 = process.version.startsWith("v23");
-export const NO_TYPE_STRIPPING_FLAG = "--no-experimental-strip-types";
--- scripts/run-playwright.js
+++ scripts/run-playwright.js
@@ -1,6 +1,13 @@
 // Credits to https://github.com/GreenAsJade/online-go.com/commit/0d24569d4d6f6187838c686a6fc7f3ee05657471#diff-8cf79278a8f33c5b40ee4195627e110f7fc7946a7aaaea7286ebcdae6195f9d3
 import { spawnSync } from "node:child_process";
-import { isNode23, NO_TYPE_STRIPPING_FLAG } from "./run-node.js";
+
+// Since with Node 23 type stripping no longer has to be explicitly activated,
+// but is standard and must be explicitly deactivated,
+// this script checks whether node is running in version 23 and if so,
+// type stripping is deactivated, since the generated GRPC client does not work with type stripping.
+
+const isNode23 = process.version.startsWith("v23");
+const NO_TYPE_STRIPPING_FLAG = "--no-experimental-strip-types";
 
 // At the moment, this workaround only needs to be applied when running playwright.
 const BASE_COMMAND = "node_modules/playwright/cli.js";
