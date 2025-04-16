c625b95064134b5ed5ba7bfba9b93a89c990be27
domienderle <dominik.enderle@uni-ulm.de>
2025-04-16 09:59:34 +0200

    feat: added the text area to define the keyword tags in

--- src/lib/components/composites/settings/project-settings/review/KeywordSettings.svelte
+++ src/lib/components/composites/settings/project-settings/review/KeywordSettings.svelte
@@ -2,7 +2,11 @@
     import SettingsSection from "$lib/components/composites/settings/SettingsSection.svelte";
     import { Label } from "$lib/components/primitives/label";
     import TagsInput from "svelte-unstyled-tags";
-    let tags = [];
+
+    const maximumAmountOfTags = 25;
+    let tags: string[] = $state([]);
+
+
 </script>
 
 <SettingsSection sectionTitle="Keywords">
@@ -10,17 +14,23 @@
         <div class="grid gap-1.5 pt-1 leading-none">
             <Label class="font-normal">
                 Define keywords that are highlighted in the abstract of a paper in the review mode.
-                <TagsInput
-                    allTagsWrapperClasses="flex flex-row items-center gap-x-2 gap-y-2 flex-wrap"
-                    componentWrapperClasses="flex flex-wrap"
-                    inputClasses="focus:outline-none focus:ring-0 pl-2"
-                    onlyUnique={true}
-                    removeTagButtonClasses="hidden group-hover:inline-block ml-2 text-black-500 hover:text-black-500 cursor-pointer"
-                    tagWrapperClasses="group flex items-center bg-gray-200 text-black px-2 py-1.5 rounded-xl"
-                    tagsInputWrapperClasses="flex items-center border-2 border-gray-200 py-2 px-3 rounded-md mt-2 h-[74px]"
-                    bind:tags
-                />
             </Label>
+            <TagsInput
+                allTagsWrapperClasses="flex flex-row items-center gap-x-2 gap-y-2 flex-wrap"
+                componentWrapperClasses="flex flex-wrap"
+                inputClasses="focus:outline-none focus:ring-0 pl-2 py-1"
+                maximumTags={maximumAmountOfTags}
+                onlyUnique={true}
+                removeTagButtonClasses="hidden group-hover:inline-block ml-2 text-black-500 hover:text-black-500 cursor-pointer"
+                tagWrapperClasses="group flex items-start bg-gray-200 text-black px-2 py-1.5 rounded-xl"
+                tagsInputWrapperClasses="flex items-start border-2 border-gray-200 py-2 px-3 rounded-md mt-2 h-[74px] overflow-auto"
+                bind:tags
+            />
+            {#if tags.length > maximumAmountOfTags-1}
+                <Label class="font-normal text-red-500">
+                    Maximal amount of tags reached.
+                </Label>
+            {/if}
         </div>
     </div>
 </SettingsSection>
