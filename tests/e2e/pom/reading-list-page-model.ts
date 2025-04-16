b5543872b5d2851d60570b2ac0a23a429d234523
Leonhard Alkewitz <leonhard@alkewitz.net>
2025-04-16 18:30:57 +0200

    refactor: change the readingList locator to from the list to all list entries

--- tests/e2e/pom/reading-list-page-model.ts
+++ tests/e2e/pom/reading-list-page-model.ts
@@ -3,12 +3,12 @@ import { type Locator, type Page } from "@playwright/test";
 export class DevReadingListPage {
     readonly page: Page;
     readonly searchBarInput: Locator;
-    readonly readingList: Locator;
+    readonly readingListEntries: Locator;
     readonly readingListEntry0: Locator;
 
     constructor(page: Page) {
         this.page = page;
-        this.readingList = page.getByRole("list");
+        this.readingListEntries = page.getByRole("listitem");
         this.searchBarInput = page.getByPlaceholder("Search");
         this.readingListEntry0 = page.getByRole("listitem").filter({
             has: page.getByRole("heading", { name: "Paper 0 on reading list", exact: true }),
