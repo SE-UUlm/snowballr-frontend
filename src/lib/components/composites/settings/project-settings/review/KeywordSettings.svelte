b020fa26632dc7e21080ea5a63c419d45226c80e
domienderle <dominik.enderle@uni-ulm.de>
2025-04-19 18:00:43 +0200

    feat: add integration tests for 'Keywords' component

--- src/lib/components/composites/settings/project-settings/review/KeywordSettings.svelte
+++ src/lib/components/composites/settings/project-settings/review/KeywordSettings.svelte
@@ -1,25 +1,38 @@
 <script lang="ts">
     import SettingsSection from "$lib/components/composites/settings/SettingsSection.svelte";
-    import { Label } from "$lib/components/primitives/label";
     import TagsInput from "svelte-unstyled-tags";
     import { toast } from "svelte-sonner";
 
-    const maximumAmountOfTags = 25;
-    const maximalTagLength = 15;
+    interface Props {
+        maximumAmountOfTags: number;
+        maximalTagLength: number;
+    }
+
+    const { maximumAmountOfTags = 25, maximalTagLength = 15 }: Props = $props();
+
     let tags: string[] = $state([]);
 
+    /**
+     * Validates the input for a tag. If the input is incorrect, the tag is not created.
+     * If a new tag input is registered, it is checked whether it is not empty, it does not exceed
+     * the maximal tag length, the maximal number of tags is not reached or the tag name already
+     * exists. In this case the tag is correct and is added to the tag input field.
+     */
     function validateTags() {
         if (tags && tags.length > 0) {
             const newTag = tags[tags.length - 1];
             if (newTag.trim().length == 0) {
                 tags.pop();
-                return toast("Empty tags are not allowed!")
+                return toast("Empty keywords are not allowed!");
             } else if (newTag.trim().length > maximalTagLength) {
                 tags.pop();
-                return toast(`Maximal tag length of ${maximalTagLength} characters exceeded!`)
+                return toast(`Maximal keyword length of ${maximalTagLength} characters exceeded!`);
             } else if (tags.length > maximumAmountOfTags) {
                 tags.pop();
-                return toast(`Maximal amount of tags reached!`)
+                return toast(`Maximal amount of keywords reached!`);
+            } else if (Array.from(tags).slice(0, tags.length - 1).includes(newTag)) {
+                tags.pop();
+                return toast(`Keyword name already exists!`);
             }
             return tags;
         }
@@ -28,18 +41,26 @@
 
 </script>
 
+<!--
+@component
+Component for adding tags. The component includes a text input field to enter the name of the tags.
+On pressing the enter button a tag gets created. Each tag has a button to remove the tag, this
+button appears if the user hovers over the tag. Both, the maximal amount of tags and the maximal
+length of a single tag are customizable.
+
+Usage:
+```svelte
+    <SettingsSection {maximumAmountOfTags} {maximalTagLength} />
+-->
 <SettingsSection sectionTitle="Keywords">
-    <div class="items-top flex flex-row space-x-2">
-        <div class="grid gap-1.5 pt-1 leading-none">
-            <Label class="font-normal">
-                Define keywords that are highlighted in the abstract of a paper in the review mode.
-            </Label>
+    <div class="items-top space-x-2">
+        <div class="grid max-w-2xl gap-1.5 pt-1 leading-none">
             <TagsInput
                 allTagsWrapperClasses="flex flex-row items-center gap-x-2 gap-y-2 flex-wrap"
-                componentWrapperClasses="flex flex-wrap"
                 inputClasses="focus:outline-none focus:ring-0 pl-2 py-1"
-                onlyUnique={true}
+                labelText="Define keywords that are highlighted in the abstract of a paper in the review mode."
                 removeTagButtonClasses="hidden group-hover:inline-block ml-2 text-black-500 hover:text-black-500 cursor-pointer"
+                showLabel={true}
                 tagWrapperClasses="group flex items-start bg-gray-200 text-black px-2 py-1.5 rounded-xl"
                 tagsInputWrapperClasses="flex items-start border-2 border-gray-200 py-2 px-3 rounded-md mt-2 h-[84px] overflow-auto"
                 bind:tags
---
+++ tests/integration/settings/project-settings/review/keyword-settings.test.ts
@@ -0,0 +1,79 @@
+import { beforeEach, describe, expect, test } from "vitest";
+import { render, screen } from "@testing-library/svelte";
+import KeywordSettings from "$lib/components/composites/settings/project-settings/review/KeywordSettings.svelte";
+import userEvent from "@testing-library/user-event";
+
+describe("KeywordSettings", () => {
+    const maximumAmountOfTags = 5;
+    beforeEach(() => {
+        render(KeywordSettings, {
+            props: {
+                maximumAmountOfTags: maximumAmountOfTags,
+                maximalTagLength: 15,
+            },
+            target: document.body,
+        });
+    });
+
+    test("It is rendered correctly", async () => {
+        expect(screen.queryByText("Keywords")).toBeInTheDocument();
+        expect(screen.getByPlaceholderText("Add a tag")).toBeInTheDocument();
+    });
+
+    test("When the input is valid, a tag is created", async () => {
+        const tagsInputField = screen.getByLabelText(
+            "Define keywords that are highlighted in the abstract of a paper in the review mode.",
+        );
+        await userEvent.type(tagsInputField, "New Tag 1");
+        await userEvent.keyboard("{Enter}");
+        expect(screen.getByText("New Tag 1")).toBeInTheDocument();
+    });
+
+    test("When the tag name already exists, the name is to long or empty, then no tag is created", async () => {
+        const tagsInputField = screen.getByLabelText(
+            "Define keywords that are highlighted in the abstract of a paper in the review mode.",
+        );
+        await userEvent.type(tagsInputField, "New Tag 1");
+        await userEvent.keyboard("{Enter}");
+        expect(screen.getByText("New Tag 1")).toBeInTheDocument();
+
+        await userEvent.type(tagsInputField, "New Tag 1");
+        await userEvent.keyboard("{Enter}");
+        expect(screen.getAllByText("New Tag 1").length).toBe(1);
+
+        await userEvent.type(tagsInputField, " ");
+        await userEvent.keyboard("{Enter}");
+        expect(screen.queryByText(" ")).not.toBeInTheDocument();
+
+        await userEvent.type(tagsInputField, "Maximal tag length exceeded");
+        await userEvent.keyboard("{Enter}");
+        expect(screen.queryByText("Maximal tag length exceeded")).not.toBeInTheDocument();
+    });
+
+    test("When the tag remove button is clicked, the according tag gets removed correctly", async () => {
+        const tagsInputField = screen.getByLabelText(
+            "Define keywords that are highlighted in the abstract of a paper in the review mode.",
+        );
+        await userEvent.type(tagsInputField, "Tag to remove");
+        await userEvent.keyboard("{Enter}");
+        const tagToRemove = screen.getByText("Tag to remove");
+        expect(tagToRemove).toBeInTheDocument();
+        expect(screen.getByRole("button", { name: "\u2715" })).toBeInTheDocument();
+        await userEvent.click(screen.getByRole("button", { name: "\u2715" }));
+        expect(tagToRemove).not.toBeInTheDocument();
+    });
+
+    test("When the maximal amount of tags is reached, no other tag is created", async () => {
+        const tagsInputField = screen.getByLabelText(
+            "Define keywords that are highlighted in the abstract of a paper in the review mode.",
+        );
+        for (let i = 0; i < maximumAmountOfTags; i++) {
+            await userEvent.type(tagsInputField, `New Tag ${i}`);
+            await userEvent.keyboard("{Enter}");
+            expect(screen.getByText(`New Tag ${i}`)).toBeInTheDocument();
+        }
+        await userEvent.type(tagsInputField, `New Tag ${maximumAmountOfTags}`);
+        await userEvent.keyboard("{Enter}");
+        expect(screen.queryByText(`New Tag ${maximumAmountOfTags}`)).not.toBeInTheDocument();
+    });
+});
