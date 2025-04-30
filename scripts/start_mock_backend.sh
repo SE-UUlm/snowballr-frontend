af92ed7cde8dcb9a82c0c48b5df7c9a024709259
Felix Schlegel <felix.schlegel@gmx.de>
2025-04-30 13:05:30 +0200

    Revert "a"
    This reverts commit 4453259007c039dd4e4dd30e98323a761b6003e9.

---
+++ scripts/start_mock_backend.sh
@@ -0,0 +1,92 @@
+#!/bin/bash
+# Script to start (multiple) mock backend(s)
+
+# Exit script on non-zero exit code
+set -e
+
+# Image for the mock backend
+IMAGE="ghcr.io/se-uulm/snowballr-mock-backend:main"
+# Name prefix for the mock backend container
+CONTAINER_PREFIX="snowballr-mock-backend"
+
+help() {
+  echo -e "usage: bash $(basename "$0") [-c] [-e] port1 [port2 ... portN] \n"
+  echo -e "Start a single or multiple mock backends. \n"
+  echo    "Options:"
+  echo    "  port               The port on which the web proxy of the mock backend should listen (must be 3000-3999)."
+  echo    "  -c                 Whether to delete all currently running mock backend containers (where names start with $CONTAINER_PREFIX)."
+  echo    "  -e                 Whether to start the mock backend with the standard example data."
+  exit 1
+}
+
+# Check, whether the port is "valid", so between 3000 and 4000
+validate_port() {
+    if [[ $1 -lt 3000 || $1 -gt 3999 ]]; then
+        echo "Error: Port $1 is out of the allowed range (3000-3999)"
+        help
+    fi
+}
+
+cleanup_running_mock_backends() {
+    echo "Stopping and removing all containers with the prefix '$CONTAINER_PREFIX' ..."
+    running_container=$(docker container ls -qa --filter "name=$CONTAINER_PREFIX")
+    if [ -n "$running_container" ]; then
+        docker stop $running_container
+        docker rm $running_container
+    else
+        echo "Warn: No container with the prefix '$CONTAINER_PREFIX' are running!"
+    fi
+}
+
+do_cleanup=false
+load_with_example_data=false
+while getopts ":hce" option; do
+    case ${option} in
+        c) do_cleanup=true ;;
+        e) load_with_example_data=true ;;
+        h | *)
+            help
+        ;;
+    esac
+done
+shift $((OPTIND - 1)) # go to next command line argument
+
+# Check for ports
+ports=("$@")
+if [ ${#ports[@]} -lt 1 ]; then
+  echo "Error: Please provide at least one port to start a mock backend."
+  help
+fi
+
+docker pull $IMAGE || { echo "Error: Failed to pull image $IMAGE"; exit 1; }
+
+if $do_cleanup; then
+    cleanup_running_mock_backends
+fi
+
+index=1
+for WEB_PROXY_PORT in "${ports[@]}"; do
+    validate_port "$WEB_PROXY_PORT"
+    NATIVE_SERVER_PORT=$((WEB_PROXY_PORT + 1000))
+    CONTAINER_NAME="$CONTAINER_PREFIX-$index"
+
+    echo "Starting mock backend $CONTAINER_NAME on ports $WEB_PROXY_PORT (web proxy) and $NATIVE_SERVER_PORT (native server) ..."
+    runArgs=()
+    if $load_with_example_data; then
+        runArgs+=( -e EXAMPLE_DATA_FILE=standardData.ts)
+    fi
+    runArgs+=( -d -q \
+                -e ENABLE_DUMMY_ADMIN=true \
+                -e GRPC_PORT="$NATIVE_SERVER_PORT" \
+                -e GRPC_WEB_PORT="$WEB_PROXY_PORT" \
+                --name "$CONTAINER_NAME" \
+                -p "$WEB_PROXY_PORT:$WEB_PROXY_PORT" -p "$NATIVE_SERVER_PORT:$NATIVE_SERVER_PORT"  \
+                "$IMAGE"
+            )
+    docker run "${runArgs[@]}" || {
+            echo "Error: Failed to start mock backend for port $WEB_PROXY_PORT"
+            help
+        }
+
+    ((index++))
+done
