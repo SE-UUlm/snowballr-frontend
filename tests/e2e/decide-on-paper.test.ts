4597d2c59da05a0c0ad8d9a23e8d312e35b30378
Leonhard Alkewitz <leonhard@alkewitz.net>
2025-05-04 20:22:57 +0200

    test: add E2E tests for deciding on a paper

---
+++ tests/e2e/decide-on-paper.test.ts
@@ -0,0 +1,144 @@
+import { test } from "./fixtures/paper-view-fixture";
+import { createPaper } from "$tests/model-builder";
+import { Project, SnowballingType } from "$lib/model/api/project";
+import { ReviewDecision } from "$lib/model/api/review";
+import { expect } from "@playwright/test";
+import type { Paper } from "$lib/model/api/paper";
+import { generateFieldMask } from "protobuf-fieldmask";
+import { Criteria } from "$tests/example-data";
+
+test.describe("Decide on paper", () => {
+    let projectId = "";
+    const projectPaperIds: string[] = [];
+    const localProjectPaperIds: string[] = [];
+
+    test.beforeAll(async ({ mockBackendService }) => {
+        const project: Project = await mockBackendService.createProject({
+            name: "Project paper decision",
+        }).response;
+        projectId = project.id;
+
+        const projectSettings: Partial<Project> = {
+            id: project.id,
+            settings: {
+                reviewMaybeAllowed: true,
+                similarityThreshold: 0,
+                fetcherApis: [],
+                snowballingType: SnowballingType.UNSPECIFIED,
+            },
+        };
+        await mockBackendService.updateProject({
+            project: Project.create(projectSettings),
+            mask: {
+                paths: generateFieldMask(projectSettings),
+            },
+        }).response;
+
+        await mockBackendService.createCriterion({
+            ...Criteria.demoCriterion1,
+            projectId: project.id,
+        });
+        await mockBackendService.createCriterion({
+            ...Criteria.demoCriterion2,
+            projectId: project.id,
+        });
+        await mockBackendService.createCriterion({
+            ...Criteria.demoCriterion3,
+            projectId: project.id,
+        });
+
+        const papers: Paper[] = await Promise.all(
+            Array.from(
+                { length: 3 },
+                (_, i) =>
+                    mockBackendService.createPaper(
+                        createPaper({ title: `Paper ${i} to decide on` }),
+                    ).response,
+            ),
+        );
+        for (const paper of papers) {
+            const projectPaper = await mockBackendService.addPaperToProject({
+                projectId: projectId,
+                paperId: paper.id,
+                stage: 0n,
+            }).response;
+            projectPaperIds.push(projectPaper.id);
+            localProjectPaperIds.push(projectPaper.localId);
+        }
+    });
+
+    test.afterAll(async ({ mockBackendService }) => {
+        for (const paperId of projectPaperIds) {
+            await mockBackendService.removePaperFromProject({ id: paperId });
+        }
+        mockBackendService.softDeleteProject({ id: projectId });
+    });
+
+    test.fixme(
+        "When the user decides on a paper by clicking the corresponding decision button, " +
+            "then a review is submitted and the next paper to review is opened",
+        async ({ page, paperViewPage }) => {
+            await page.goto(`project/${projectId}/paper/${localProjectPaperIds[0]}`);
+
+            await paperViewPage.decideOnPaper(ReviewDecision.ACCEPTED);
+            /// TODO: add check for automatic navigation
+        },
+    );
+
+    test.fixme(
+        "When the user presses one of the shortcuts for a decision, " +
+            "then a review is submitted and the next paper to review is opened",
+        async ({ page, paperViewPage }) => {
+            await page.goto(`project/${projectId}/paper/${localProjectPaperIds[1]}`);
+            await expect(paperViewPage.acceptButton).toBeVisible();
+
+            await page.keyboard.press("Control+a");
+            /// TODO: add check for automatic navigation
+        },
+    );
+
+    test(
+        "When the user selects certain review criteria and clicks a decision button, " +
+            "then a review is submitted and the decision is shown after a reload.",
+        async ({ page, paperViewPage }) => {
+            await page.goto(`project/${projectId}/paper/${localProjectPaperIds[2]}`);
+
+            await paperViewPage.decideOnPaper(ReviewDecision.ACCEPTED);
+            await expect(page.getByText("Successfully submitted a review.")).toBeVisible();
+
+            await page.reload();
+
+            await expect(paperViewPage.acceptButton).toContainClass("ring-1");
+            await expect(paperViewPage.declineButton).not.toContainClass("ring-1");
+            await expect(paperViewPage.maybeButton).not.toContainClass("ring-1");
+
+            await expect(
+                paperViewPage.exampleHardExclusionCriterion.getByRole("checkbox"),
+            ).not.toBeChecked();
+            await expect(
+                paperViewPage.exampleInclusionCriterion.getByRole("checkbox"),
+            ).toBeChecked();
+        },
+    );
+
+    test(
+        "When the user opens a project paper in review mode that was already reviewed by " +
+            "the user, then it is not possible to change any decision (including review criteria).",
+        async ({ page, paperViewPage }) => {
+            await page.goto(`project/${projectId}/paper/${localProjectPaperIds[2]}`);
+
+            await page.reload();
+
+            await expect(paperViewPage.acceptButton).toBeDisabled();
+            await expect(paperViewPage.declineButton).toBeDisabled();
+            await expect(paperViewPage.maybeButton).toBeDisabled();
+
+            await expect(
+                paperViewPage.exampleInclusionCriterion.getByRole("checkbox"),
+            ).toBeDisabled();
+            await expect(
+                paperViewPage.exampleInclusionCriterion.getByRole("checkbox"),
+            ).toBeChecked();
+        },
+    );
+});
--- tests/e2e/pom/paper-view-model.ts
+++ tests/e2e/pom/paper-view-model.ts
@@ -1,9 +1,15 @@
 import { expect, type Locator, type Page } from "@playwright/test";
+import { ReviewDecision } from "$lib/model/api/review";
 
 export class DevPaperViewPage {
     readonly page: Page;
     readonly allListEntries: Locator;
     readonly referenceListEntry0: Locator;
+    readonly declineButton: Locator;
+    readonly maybeButton: Locator;
+    readonly acceptButton: Locator;
+    readonly exampleInclusionCriterion: Locator;
+    readonly exampleHardExclusionCriterion: Locator;
 
     constructor(page: Page) {
         this.page = page;
@@ -13,6 +19,11 @@ export class DevPaperViewPage {
                 name: "Paper 1 to be referenced",
             })
             .first();
+        this.declineButton = page.getByRole("button", { name: "Decline", exact: false });
+        this.maybeButton = page.getByRole("button", { name: "Maybe", exact: false });
+        this.acceptButton = page.getByRole("button", { name: "Accept", exact: false });
+        this.exampleInclusionCriterion = page.getByRole("listitem").filter({ hasText: /I/ });
+        this.exampleHardExclusionCriterion = page.getByRole("listitem").filter({ hasText: /HE/ });
     }
 
     /**
@@ -30,5 +41,31 @@ export class DevPaperViewPage {
      */
     async navigateToReferencedPaper() {
         await this.referenceListEntry0.click();
+    }
+
+    /**
+     * Decides on a paper by clicking the corresponding button.
+     *
+     * @remarks If the paper is accepted, the inclusion criterion is selected too,
+     * in case the paper is declined, the hard exclusion criterion is selected.
+     *
+     * @param decision - The decision that should be submitted for the paper
+     */
+    async decideOnPaper(decision: ReviewDecision) {
+        switch (decision) {
+            case ReviewDecision.ACCEPTED:
+                await this.exampleInclusionCriterion.getByRole("checkbox").check();
+                await this.acceptButton.click();
+                break;
+            case ReviewDecision.MAYBE:
+                await this.maybeButton.click();
+                break;
+            case ReviewDecision.DECLINED:
+                await this.exampleHardExclusionCriterion.getByRole("checkbox").check();
+                await this.declineButton.click();
+                break;
+            default:
+                console.error("Unknown decision", decision);
+        }
     }
 }
