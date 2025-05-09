faa5236976daf26cd8509995488a25acc63966ca
domienderle <dominik.enderle@uni-ulm.de>
2025-05-09 16:50:24 +0200

    feat: add e2e tests

---
+++ tests/e2e/pom/paper-view-model.ts
@@ -0,0 +1,34 @@
+import { expect, type Locator, type Page } from "@playwright/test";
+
+export class DevPaperViewPage {
+    readonly page: Page;
+    readonly allListEntries: Locator;
+    readonly referenceListEntry0: Locator;
+
+    constructor(page: Page) {
+        this.page = page;
+        this.allListEntries = page.getByRole("listitem");
+        this.referenceListEntry0 = this.allListEntries
+            .getByRole("button", {
+                name: "Paper 0 to be referenced",
+            })
+            .first();
+    }
+
+    /**
+     * Navigates to a certain paper view and ensures the page is loaded.
+     *
+     * @param paperId - The local project paper id
+     */
+    async openPaperView(paperId: string) {
+        await this.page.goto(`/paper/${paperId}`);
+        await expect(this.referenceListEntry0).toBeVisible();
+    }
+
+    /**
+     * Navigates to the first referenced paper and ensures the page is loaded.
+     */
+    async navigateToReferencedPaper() {
+        await this.referenceListEntry0.click();
+    }
+}
