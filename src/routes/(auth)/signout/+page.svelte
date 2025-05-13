475b43c3177f83aaf35706524484dec28fa85cc1
Luca Schlecker <luca.schlecker@hotmail.com>
2025-05-13 12:08:45 +0200

    fix: move signout out of a page load function and into a compontent to mitigate prefetching issues

---
+++ src/routes/(auth)/signout/+page.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+    import { backendService } from "$lib/grpc-api";
+    import { goto } from "$app/navigation";
+    import { onMount } from "svelte";
+    import { Nothing } from "$lib/model/api/base";
+
+    onMount(async () => {
+        try {
+            await backendService.logout(Nothing);
+        } catch (e) {
+            console.error("There was an error logging out: ", e);
+        } finally {
+            await goto("/signin");
+        }
+    });
+</script>
--- src/routes/(auth)/signout/+page.ts
+++
@@ -1,8 +0,0 @@
-import type { PageLoad } from "./$types";
-import { backendService } from "$lib/grpc-api";
-import { goto } from "$app/navigation";
-
-export const load: PageLoad = async () => {
-    await backendService.logout({});
-    goto("/signin");
-};
