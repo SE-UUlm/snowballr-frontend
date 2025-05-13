7cbe45ecdd26a42c8cc3dc2dfb473fd7aac422cc
Moritz Wieland <moritz.wieland@googlemail.com>
2025-05-13 19:32:20 +0200

    fix: split change name e2e test to reduce flakiness

--- tests/e2e/change-username-settings.test.ts
+++ tests/e2e/change-username-settings.test.ts
@@ -29,7 +29,7 @@ test.describe("Changing username", () => {
         await expect(page.getByRole("button", { name: "ZZ", exact: true })).toBeVisible();
     });
 
-    test("When the user enters an invalid first or last name, the new name should not be updated.", async ({
+    test("When the user enters an invalid first name, the new name should not be updated.", async ({
         page,
         accountSettingsPage,
     }) => {
@@ -39,12 +39,21 @@ test.describe("Changing username", () => {
         await expect(
             page.getByText("First Name must contain at least 1 non-whitespace character"),
         ).toBeVisible();
+
+        await expect(page.getByRole("button", { name: "ZZ", exact: true })).toBeVisible();
+    });
+
+    test("When the user enters an invalid last name, the new name should not be updated.", async ({
+        page,
+        accountSettingsPage,
+    }) => {
         await expect(page.getByRole("button", { name: "ZZ", exact: true })).toBeVisible();
 
         accountSettingsPage.changeUsername("Alpha", " ");
         await expect(
             page.getByText("Last Name must contain at least 1 non-whitespace character"),
         ).toBeVisible();
+
         await expect(page.getByRole("button", { name: "ZZ", exact: true })).toBeVisible();
     });
 });
