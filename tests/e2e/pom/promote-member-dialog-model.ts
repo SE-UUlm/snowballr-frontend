189885eed3f5acb33a91fdc73fa37e9cc0e3862d
Felix Schlegel <felix.schlegel@gmx.de>
2025-05-14 17:02:56 +0200

    test: add e2e tests for promote project members

---
+++ tests/e2e/pom/promote-member-dialog-model.ts
@@ -0,0 +1,56 @@
+import { getName } from "$lib/utils/common-helper";
+import { expect, type Locator, type Page } from "@playwright/test";
+
+export class DevPromoteMemberDialog {
+    readonly page: Page;
+    readonly dialog: Locator;
+    readonly openButton: Locator;
+    readonly confirmButton: Locator;
+    readonly cancelButton: Locator;
+
+    constructor(page: Page, user: { firstName?: string; lastName?: string; email: string }) {
+        let displayName: string;
+        if (user.firstName && user.lastName) {
+            displayName = getName({ firstName: user.firstName, lastName: user.lastName });
+        } else {
+            displayName = user.email;
+        }
+
+        this.page = page;
+        this.dialog = page.getByRole("alertdialog", {
+            name: `Promote ${displayName} to a Project Admin`,
+        });
+        this.openButton = page.locator(`button[aria-label="Promote member ${user.email}"]`);
+        this.confirmButton = this.dialog.getByRole("button", {
+            name: `Promote Member to a Project Admin`,
+        });
+        this.cancelButton = this.dialog.getByRole("button", { name: "Cancel" });
+    }
+
+    /**
+     * Opens the dialog for promoting a user to an admin.
+     */
+    async open() {
+        await expect(this.dialog).not.toBeVisible();
+        await this.openButton.click();
+        await expect(this.dialog).toBeVisible();
+    }
+
+    /**
+     * Promotes the user to an admin.
+     */
+    async promote() {
+        await expect(this.dialog).toBeVisible();
+        await this.confirmButton.click();
+        await expect(this.dialog).not.toBeVisible();
+    }
+
+    /**
+     * Cancels the promotion of the user to an admin.
+     */
+    async cancel() {
+        await expect(this.dialog).toBeVisible();
+        await this.cancelButton.click();
+        await expect(this.dialog).not.toBeVisible();
+    }
+}
---
+++ tests/e2e/promote-members.test.ts
@@ -0,0 +1,91 @@
+import { expect } from "@playwright/test";
+import { test } from "./fixtures/project-members-settings-fixtures";
+import type { DevProjectMemberSettingsPage } from "./pom/project-member-settings-page-model";
+import { DevPromoteMemberDialog } from "./pom/promote-member-dialog-model";
+import { DevRemoveMemberDialog } from "./pom/remove-member-dialog-model";
+
+test.describe("Promoting members to project admins", () => {
+    async function inviteUser(
+        email: string,
+        projectMembersSettingsPage: DevProjectMemberSettingsPage,
+        useSuggestion: boolean = false,
+    ) {
+        await projectMembersSettingsPage.openInviteUsersDialog();
+        await projectMembersSettingsPage.inviteUser(email, useSuggestion);
+        await projectMembersSettingsPage.inviteUsersDialogButton.click();
+        await expect(projectMembersSettingsPage.inviteUsersDialog).not.toBeVisible();
+        await projectMembersSettingsPage.assertNumberOfProjectMembers({
+            all: 2,
+            admins: 1,
+            members: 1,
+            invitees: 1,
+            invitedAdmins: 0,
+            invitedMembers: 1,
+        });
+    }
+
+    test("When the current user is listed, then they can't promote themselves to a project admin.", async ({
+        page,
+        projectMembersSettingsPage,
+        user,
+    }) => {
+        // obligatory check for the current user, so that projectMembersSettingsPage is used
+        await projectMembersSettingsPage.checkForUser(user!.email, "email");
+
+        const dialog = new DevPromoteMemberDialog(page, user!);
+        await expect(dialog.openButton).toBeVisible();
+        await expect(dialog.openButton).toBeDisabled();
+        await expect(dialog.openButton).toHaveText("Role: Admin");
+    });
+
+    test("When the promote user dialog is opened and cancelled, then the user is not promoted.", async ({
+        page,
+        projectMembersSettingsPage,
+    }) => {
+        const userEmail = "john.doe@example.com";
+        await inviteUser(userEmail, projectMembersSettingsPage);
+
+        const dialog = new DevPromoteMemberDialog(page, { email: userEmail });
+
+        await dialog.open();
+        await dialog.cancel();
+        await expect(dialog.dialog).not.toBeVisible();
+        await projectMembersSettingsPage.checkForUser(userEmail, "email");
+        await projectMembersSettingsPage.assertNumberOfProjectMembers({
+            all: 2,
+            admins: 1,
+            members: 1,
+            invitees: 1,
+            invitedMembers: 1,
+            invitedAdmins: 0,
+        });
+    });
+
+    // Currently, we cannot promote invited users. Only when they are in the project.
+    test.fail(
+        "When other users are listed, then they can be promoted to project admins.",
+        async ({ projectMembersSettingsPage, page }) => {
+            const userEmail = "john.doe@example.com";
+            await inviteUser(userEmail, projectMembersSettingsPage);
+
+            const dialog = new DevPromoteMemberDialog(page, { email: userEmail });
+
+            await dialog.open();
+            await dialog.promote();
+            await expect(dialog.dialog).not.toBeVisible();
+            await expect(page.getByText(`Promoted John Doe to a Project Admin`)).toBeVisible();
+            await projectMembersSettingsPage.checkForUser(userEmail, "email");
+            await expect(dialog.openButton).toHaveText("Role: Admin");
+            const removeMemberDialog = new DevRemoveMemberDialog(page, { email: userEmail });
+            await expect(removeMemberDialog.openButton).toBeDisabled();
+            await projectMembersSettingsPage.assertNumberOfProjectMembers({
+                all: 2,
+                admins: 2,
+                members: 0,
+                invitees: 1,
+                invitedAdmins: 1,
+                invitedMembers: 0,
+            });
+        },
+    );
+});
