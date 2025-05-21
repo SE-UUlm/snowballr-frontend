007122fcadf5268c88ea568741e5ac539053dd76
Leonhard Alkewitz <leonhard@alkewitz.net>
2025-05-21 15:31:04 +0200

    test: add unit tests for util filter functions
    These filter functions are used to filter user, paper and project paper by search text and / or custom filter arrays

--- tests/unit/utils/filters.test.ts
+++ tests/unit/utils/filters.test.ts
@@ -1,8 +1,30 @@
 import { describe, expect, test } from "vitest";
-import { ProjectPapers } from "$tests/example-data";
-import { filterProjectPapers } from "$lib/utils/filters";
+import { Papers, ProjectPapers, Users } from "$tests/example-data";
+import { filterPapers, filterProjectPapers, filterUsers } from "$lib/utils/filters";
+import { PaperDecision, type Project_Paper } from "$lib/model/api/project";
+import { createPaper, createProjectPaper, createReview } from "$tests/model-builder";
 
 describe("Custom filter", () => {
+    test(
+        "When papers should be filtered, but no search is provided, " +
+            "then the same list of papers is returned ",
+        async () => {
+            const papers = [Papers.demoPaper1, Papers.demoPaper2, Papers.demoPaper3];
+
+            expect(filterPapers(papers, "")).toStrictEqual(papers);
+        },
+    );
+
+    test(
+        "When papers are filtered, only papers where the id, title or authors match " +
+            "the search string - considering the FZF matching - are returned.",
+        async () => {
+            const papers = [Papers.demoPaper1, Papers.demoPaper2, Papers.demoPaper3];
+
+            expect(filterPapers(papers, "2 J")).toStrictEqual([Papers.demoPaper3]);
+        },
+    );
+
     test(
         "When project papers should be filtered, but neither a filter nor a search text is provided," +
             "then the same list of project papers is returned",
@@ -13,7 +35,127 @@ describe("Custom filter", () => {
                 ProjectPapers.demoProjectPaper3,
             ];
 
-            expect(filterProjectPapers(projectPapers)).toBe(projectPapers);
+            expect(filterProjectPapers(projectPapers)).toStrictEqual(projectPapers);
         },
     );
+
+    test(
+        "When project papers are filtered by search text, only papers where the id, title or " +
+            "authors match the search string - considering the FZF matching - are returned.",
+        () => {
+            const projectPapers = [
+                ProjectPapers.demoProjectPaper1,
+                ProjectPapers.demoProjectPaper2,
+                ProjectPapers.demoProjectPaper3,
+            ];
+
+            expect(
+                filterProjectPapers(projectPapers, undefined, "TypeScript Performance"),
+            ).toStrictEqual([ProjectPapers.demoProjectPaper1]);
+
+            // test separate search starting with '#' to search directly by paper id
+            expect(filterProjectPapers(projectPapers, undefined, "#0")).toStrictEqual([
+                ProjectPapers.demoProjectPaper1,
+            ]);
+        },
+    );
+
+    test(
+        "When project papers are filtered by specific filter arrays, only papers conforming all" +
+            "non-empty filter arrays are returned.",
+        () => {
+            const projectPapers = [
+                ProjectPapers.demoProjectPaper1,
+                ProjectPapers.demoProjectPaper2,
+                ProjectPapers.demoProjectPaper3,
+            ];
+
+            const filters = {
+                stages: [],
+                reviewers: [],
+                publishers: [],
+                years: [],
+                decisions: [String(PaperDecision.UNREVIEWED)],
+                criteria: [],
+            };
+
+            expect(filterProjectPapers(projectPapers, filters)).toStrictEqual([
+                ProjectPapers.demoProjectPaper1,
+            ]);
+        },
+    );
+
+    test(
+        "When users should be filtered, but no search is provided, " +
+            "then the same list of users is returned ",
+        async () => {
+            const users = [Users.johnDoe, Users.janeDoe, Users.aliceSmith, Users.bobJohnson];
+
+            expect(filterUsers(users, "")).toStrictEqual(users);
+        },
+    );
+
+    test(
+        "When users are filtered, only users matching the search string - " +
+            "considering the FZF matching - are returned.",
+        async () => {
+            const users = [Users.johnDoe, Users.janeDoe, Users.aliceSmith, Users.bobJohnson];
+
+            expect(filterUsers(users, "Doe")).toStrictEqual([Users.johnDoe, Users.janeDoe]);
+        },
+    );
+
+    test.each([
+        { stage: 1n },
+        { reviews: [createReview({ userId: "0", selectedCriteriaIds: ["2"] })] },
+        { paper: createPaper({ publisher: "ACM", year: 2013 }) },
+        { paper: createPaper({ publisher: "IEEE Explore", year: 1999 }) },
+        { decision: PaperDecision.ACCEPTED },
+        { reviews: [createReview({ userId: "1", selectedCriteriaIds: ["0"] })] },
+    ])(
+        "When a project paper does not match all filters, then it is filtered out " +
+            "(failing property: $0)",
+        (property: Partial<Project_Paper>) => {
+            const filters = {
+                stages: ["0"],
+                reviewers: ["1"],
+                publishers: ["IEEE Explore"],
+                years: ["2013"],
+                decisions: [String(PaperDecision.UNREVIEWED)],
+                criteria: ["2"],
+            };
+
+            const projectPaper = createProjectPaper({
+                stage: 0n,
+                reviews: [createReview({ userId: "1", selectedCriteriaIds: ["2"] })],
+                decision: PaperDecision.UNREVIEWED,
+                paper: createPaper({ publisher: "IEEE Explore", year: 2013 }),
+            });
+            expect(filterProjectPapers([{ ...projectPaper, ...property }], filters)).toStrictEqual(
+                [],
+            );
+        },
+    );
+
+    test("When a project paper match all filters, then it is not filtered out", () => {
+        const filters = {
+            stages: ["0"],
+            reviewers: ["1"],
+            publishers: ["IEEE Explore"],
+            years: ["2013"],
+            decisions: [String(PaperDecision.UNREVIEWED)],
+            criteria: ["2"],
+        };
+
+        const matchingProjectPaper = createProjectPaper({
+            stage: 0n,
+            reviews: [createReview({ userId: "1", selectedCriteriaIds: ["2"] })],
+            decision: PaperDecision.UNREVIEWED,
+            paper: createPaper({ publisher: "IEEE Explore", year: 2013 }),
+        });
+
+        expect(filterProjectPapers([matchingProjectPaper], filters)).toStrictEqual([
+            matchingProjectPaper,
+        ]);
+    });
 });
