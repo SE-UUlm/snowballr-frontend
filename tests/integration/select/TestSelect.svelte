0861d9e1c2a8f6a98643afe85cfd64981810b090
Leonhard Alkewitz <leonhard@alkewitz.net>
2025-05-22 00:31:43 +0200

    test: update integration tests for 'Select' component
    As the select component now has the option to select / unselect multiple options and determine the label differently, the tests had to be adapted and extended to test the new features.

---
+++ tests/integration/select/TestSelect.svelte
@@ -0,0 +1,7 @@
+<script lang="ts">
+    import Select from "$lib/components/composites/select/Select.svelte";
+
+    let { options, selectedValues } = $props();
+</script>
+
+<Select {options} bind:selectedValues={$selectedValues} />
