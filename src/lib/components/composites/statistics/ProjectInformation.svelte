0049bf22508afdccf9b027287ce3de45fb70b916
Moritz Wieland <moritz.wieland@googlemail.com>
2025-05-24 09:36:28 +0200

    chore: remove inappropriate comment

--- src/lib/components/composites/statistics/ProjectInformation.svelte
+++ src/lib/components/composites/statistics/ProjectInformation.svelte
@@ -3,10 +3,6 @@
     import { pluralize } from "$lib/utils/common-helper";
     import ErrorIndicator from "$lib/components/composites/utils/ErrorIndicator.svelte";
 
-    /**
-     * Contains interfaces or types that specify the interface of components.
-     * These are not defined locally so that these types can be used at the point where the data for the components is loaded or created too.
-     */
     export interface ProjectInformationInterface {
         projectName: string;
         projectStart: Date;
