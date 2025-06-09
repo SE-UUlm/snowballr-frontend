0e2da6abd829b6e7ef85c6aed880c1706c709e6b
domienderle <dominik.enderle@uni-ulm.de>
2025-06-09 13:25:16 +0200

    feat: add e2e tests for archived projects

---
+++ tests/e2e/archivedprojects/archived-projects-page.test.ts
@@ -0,0 +1,17 @@
+import { test } from "./archived-projects-page-fixture";
+import { expect } from "@playwright/test";
+
+test.describe("Archived Projects Tests", () => {
+    test("When navigating to the archived projects, then the page is displayed", async ({
+        page,
+        archivedProjectsPage,
+        navigationBar,
+    }) => {
+        // Directly navigate to the archived projects
+        await page.goto("/");
+
+        await navigationBar.getUserAvatarButton().click();
+        await navigationBar.getArchivedProjectsLink().click();
+        await expect(archivedProjectsPage.heading).toBeVisible();
+    });
+});
