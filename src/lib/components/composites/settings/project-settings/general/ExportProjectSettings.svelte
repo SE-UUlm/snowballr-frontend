cd908e3d4c2e1418410dc2471dc3153c72db0959
Felix Schlegel <felix.schlegel@gmx.de>
2025-11-21 11:31:28 +0100

    feat: add error handling for project export

--- src/lib/components/composites/settings/project-settings/general/ExportProjectSettings.svelte
+++ src/lib/components/composites/settings/project-settings/general/ExportProjectSettings.svelte
@@ -8,6 +8,8 @@
     import { loadingWrapper, pluralize } from "$lib/utils/common-helper";
     import { downloadBlob } from "$lib/utils/download-file";
     import SingleSelect from "$lib/components/composites/select/SingleSelect.svelte";
+    import { createActionError, type ActionError } from "$lib/model/action-error";
+    import ActionErrorAlert from "$lib/components/composites/utils/ActionErrorAlert.svelte";
 
     interface Props {
         projectId: string;
@@ -19,11 +21,21 @@
     let formats = $state<string[]>([]);
     let selectedFormat = $state<string | undefined>(undefined);
     const loadingExport = $state({ value: false });
+    let exportProjectError: ActionError = $state(undefined);
 
     const formatsPromise = backendService
         .getAvailableExportFormats(Nothing)
         .response.then((availableFormats) => {
             formats = availableFormats.formats;
+        })
+        .catch((error) => {
+            exportProjectError = createActionError(
+                "Failed to Load the Export Formats",
+                {
+                    action: "loading the export formats",
+                },
+                error,
+            );
         })
         .finally(() => {
             loadingFormats = false;
@@ -35,6 +47,7 @@
         }
 
         loadingExport.value = true;
+        exportProjectError = undefined;
 
         const request: ExportRequest = {
             id: projectId,
@@ -45,6 +58,15 @@
             .exportProject(request)
             .response.then(({ data, fileName }) => {
                 downloadBlob(data, fileName);
+            })
+            .catch((error) => {
+                exportProjectError = createActionError(
+                    "Failed to Export the Project",
+                    {
+                        action: "exporting the project",
+                    },
+                    error,
+                );
             })
             .finally(() => {
                 loadingExport.value = false;
@@ -80,4 +102,5 @@
             onclick={(args) => loadingWrapper(loadingExport, exportProject, args)}
         />
     </div>
+    <ActionErrorAlert class="max-w-100 md:max-w-150" error={exportProjectError} />
 </SettingsSection>
