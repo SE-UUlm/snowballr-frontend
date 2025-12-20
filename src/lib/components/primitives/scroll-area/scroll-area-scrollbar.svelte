d60f5a17fae8dc35b48cee2a67207457595c34a2
Felix Schlegel <felix.schlegel@gmx.de>
2025-12-20 18:23:00 +0100

    feat: add ScrollArea component

---
+++ src/lib/components/primitives/scroll-area/scroll-area-scrollbar.svelte
@@ -0,0 +1,31 @@
+<script lang="ts">
+    import { ScrollArea as ScrollAreaPrimitive, type WithoutChild } from "bits-ui";
+    import { cn } from "$lib/utils/shadcn-helper.js";
+
+    let {
+        ref = $bindable(null),
+        class: className,
+        orientation = "vertical",
+        children,
+        ...restProps
+    }: WithoutChild<ScrollAreaPrimitive.ScrollbarProps> = $props();
+</script>
+
+<ScrollAreaPrimitive.Scrollbar
+    class={cn(
+        "flex touch-none p-px transition-colors select-none",
+        orientation === "vertical" && "h-full w-2.5 border-s border-s-transparent",
+        orientation === "horizontal" && "h-2.5 flex-col border-t border-t-transparent",
+        className,
+    )}
+    data-slot="scroll-area-scrollbar"
+    {orientation}
+    bind:ref
+    {...restProps}
+>
+    {@render children?.()}
+    <ScrollAreaPrimitive.Thumb
+        class="bg-border relative flex-1 rounded-full"
+        data-slot="scroll-area-thumb"
+    />
+</ScrollAreaPrimitive.Scrollbar>
